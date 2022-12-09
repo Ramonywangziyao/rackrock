52138b625de5bac7c3ee32cf408e1257fdff6378
Ziyao Wang <tonywangziyao@gmail.com>
2022-12-09 23:40:11 +0800

    all tested

--- controller/ReportController.go
+++ controller/ReportController.go
@@ -305,5 +305,83 @@ func (con ReportController) GetShareLink(c *gin.Context) (res model.RockResp) {
 }
 
 func (con ReportController) ExportSaleDetail(c *gin.Context) (res model.RockResp) {
-	return
+	loginUser := context.GetLoginUser(c)
+	userId := loginUser.ID
+
+	eventIdStr := c.Query("eventId")
+	if len(eventIdStr) == 0 {
+		// 没有传场次
+		con.Error(c, model.RequestParameterMissingErrorCode, model.RequestParameterMissingError)
+		return model.RockResp{
+			Code:    model.RequestParameterMissingErrorCode,
+			Message: model.RequestParameterMissingError,
+			Data:    nil,
+		}
+	}
+	eventId, err := utils.ConvertStringToUint64(eventIdStr)
+	if err != nil {
+		con.Error(c, model.DataTypeConversionErrorCode, model.DataTypeConversionError)
+		return model.RockResp{
+			Code:    model.DataTypeConversionErrorCode,
+			Message: model.DataTypeConversionError,
+			Data:    nil,
+		}
+	}
+
+	// 检查用户是否对该场次有权限
+	accessLevel, err := service.GetUserAccessLevel(userId)
+	if err != nil {
+		con.Error(c, model.SqlQueryErrorCode, model.SqlQueryError)
+		return model.RockResp{
+			Code:    model.SqlQueryErrorCode,
+			Message: model.SqlQueryError,
+			Data:    nil,
+		}
+	}
+
+	event, err := service.GetEvent(eventId)
+	if err != nil {
+		con.Error(c, model.SqlQueryErrorCode, fmt.Sprintf("%s : event, %s", model.SqlQueryError, err))
+		return model.RockResp{
+			Code:    model.SqlQueryErrorCode,
+			Message: fmt.Sprintf("%s : event, %s", model.SqlQueryError, err),
+			Data:    nil,
+		}
+	}
+	// 管理员跳过
+	if accessLevel != model.ADMIN && event.UserId != userId && event.CreatorId != userId {
+		con.Error(c, model.NotAuthorizedErrorCode, model.NotAuthorizedError)
+		return model.RockResp{
+			Code:    model.NotAuthorizedErrorCode,
+			Message: model.NotAuthorizedError,
+			Data:    nil,
+		}
+	}
+
+	// 检查报告页状态
+	if event.ReportStatus == 0 {
+		con.Error(c, model.ReportNotReadyErrorCode, model.ReportNotReadyError)
+		return model.RockResp{
+			Code:    model.ReportNotReadyErrorCode,
+			Message: model.ReportNotReadyError,
+			Data:    nil,
+		}
+	}
+
+	startTime := c.Query("startTime")
+	endTime := c.Query("endTime")
+	brand := c.Query("brand")
+	source := c.Query("source")
+
+	xlsx := service.GetSaleDetailSheet(event, startTime, endTime, brand, source)
+	c.Header("Content-Type", "application/octet-stream")
+	c.Header("Content-Disposition", "attachment; filename="+"sale.xlsx")
+	c.Header("Content-Transfer-Encoding", "binary")
+	_ = xlsx.Write(c.Writer)
+
+	return model.RockResp{
+		Code:    model.OK,
+		Message: model.RequestSuccessMsg,
+		Data:    nil,
+	}
 }
