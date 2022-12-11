a184f6d620b004177a5b0d8dccb41472d066e61f
dylan <tonywangziyao@gmail.com>
2022-12-11 13:50:01 +0800

    fixed dashboard bug

--- service/DashboardService.go
+++ service/DashboardService.go
@@ -18,22 +18,28 @@ func GetDashboardInfo(userId uint64) (model.DashboardBasicResponse, error) {
 		return model.DashboardBasicResponse{}, errors.New(model.SqlQueryError)
 	}
 
-	events, err := repo.GetEventIdsByUserId(component.DB, userId)
-	if err != nil {
-		return model.DashboardBasicResponse{}, errors.New(model.SqlQueryError)
-	}
+	if eventCount > 0 {
+		events, err := repo.GetEventIdsByUserId(component.DB, userId)
+		if err != nil {
+			return model.DashboardBasicResponse{}, errors.New(model.SqlQueryError)
+		}
 
-	amountSoldCount, err := repo.GetTotalAmountSoldByEventIds(component.DB, events)
-	if err != nil {
-		return model.DashboardBasicResponse{}, errors.New(model.SqlQueryError)
-	}
+		amountSoldCount, err := repo.GetTotalAmountSoldByEventIds(component.DB, events)
+		if err != nil {
+			return model.DashboardBasicResponse{}, errors.New(model.SqlQueryError)
+		}
 
-	itemSoldCount, err := repo.GetTotalItemSoldByEventIds(component.DB, events)
-	if err != nil {
-		return model.DashboardBasicResponse{}, errors.New(model.SqlQueryError)
+		itemSoldCount, err := repo.GetTotalItemSoldByEventIds(component.DB, events)
+		if err != nil {
+			return model.DashboardBasicResponse{}, errors.New(model.SqlQueryError)
+		}
+
+		return model.DashboardBasicResponse{
+			nickname, eventCount, amountSoldCount, itemSoldCount,
+		}, nil
 	}
 
 	return model.DashboardBasicResponse{
-		nickname, eventCount, amountSoldCount, itemSoldCount,
+		nickname, eventCount, 0, 0,
 	}, nil
 }
