f20d2731ec2302db426e37bb7a2b841e05f9132a
dylan <tonywangziyao@gmail.com>
2022-12-28 13:25:12 +0800

    added season and color

--- service/ExcelService.go
+++ service/ExcelService.go
@@ -78,7 +78,6 @@ func ReadEventItemFile(file *excelize.File, eventId string) error {
 				break
 			case "M":
 				discount, err := strconv.ParseFloat(cellValue, 64)
-				fmt.Println(fmt.Sprintf("discount %f", discount))
 				item.Discount = float32(discount)
 				if err != nil {
 					fmt.Sprintf("Error: item discount conversion %s", err)
